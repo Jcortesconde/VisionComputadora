2538933c0e32343a6ae921d966460b99bb897f1f
Zamboni Gianfranco <gzamboni@ws8.labo3>
2018-10-02 21:13:21 -0300

    no anda el convol

--- Practicas/Practica3/add_padding.m
+++ Practicas/Practica3/add_padding.m
@@ -1,16 +1,17 @@
-function res = add_padding(img, mask_size):
+function res = add_padding(img, mask_size)
   r = floor(mask_size(1)/2);
   c = floor(mask_size(2)/2);
- 
-  upper_padding = ones(r,1)*img(1,:);
-  lower_padding = ones(r,1)*img(:,size(img)(1));
   
+  upper_padding = ones(r,1)'*img(1,:);
+  lower_padding = ones(r,1)'*img(size(img)(1),:);
+ 
   res = [ upper_padding ; img; lower_padding ];
  
   left_padding = res(:,1)*ones(c,1)';
   right_padding = res(:,size(res)(2))*ones(c,1)';
   
-  res = [ left_padding res right_padding ]
+  res = [ left_padding res right_padding ];
 
+  imshow(res)
  endfunction
   
--- Practicas/Practica3/convol.m
+++ Practicas/Practica3/convol.m
@@ -1,5 +1,5 @@
-function res = convol(img, mask):
-  mask_size = size(mascara);
+function res = convol(img, mask)
+  mask_size = size(mask);
   img_size = size(img);
   res = img;
   
@@ -7,11 +7,11 @@ function res = convol(img, mask):
   r = floor(mask_size(1)/2);
   c = floor(mask_size(2)/2);
 
-  for i = r_start : img_size(1)
-    for j = c_start : img_size(2)
-      submatrix = A(i-r:i+r, i-c:i+c);
+  for i = r+1 : padded_img(1) - r - 1
+    for j = c+1 : padded_img(2) - c - 1
+      submatrix = padded_img(i-r:i+r, i-c:i+c);
       submatrix = submatrix .* mask;
-      res = sum(sum(submatrix))
+      res = sum(sum(submatrix));
     endfor    
   endfor
 endfunction
