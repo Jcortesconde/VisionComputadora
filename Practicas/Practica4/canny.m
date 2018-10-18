ae1b5670b5f21ea616a28555893f9d647292eb0a
javier <javiercortescondetito@gmail.com>
2018-10-18 10:25:07 -0300

    mas velocidad en canny para mas placer

--- Practicas/Practica4/canny.m
+++ Practicas/Practica4/canny.m
@@ -1,14 +1,13 @@
-function edge = canny(img, mask, gradient_method, low_threshold, high_threshold)
-  %mascara es o la identidad en la convolusion si no tiene ruido
-  % o un pasabajo para sacarlo, recomendamos el gaussiano. (depende el ruido que
-  % pensamos que tiene la imagen.
+function edge = canny(img, gradient_method,size_gauss, low_threshold, high_threshold)
+ gauss = fspecial("gaussian", size_gauss);
  
- aux = uint8(conv2(img, mask, 'same'));
+ aux = imfilter(img, gauss);
  [aux, grad_dir] = intesity_gradient(aux, gradient_method);
  grad_dir = compress_grad_dir(grad_dir);
  aux = non_maximum(aux,grad_dir);
- aux = hysteresis(aux, grad_dir, low_threshold, high_threshold);
+ logic_borders = hysteresis(aux, grad_dir, low_threshold, high_threshold);
  
 % edge = aux;
- edge = deriche(aux, grad_dir);
+ %logic_borders = deriche(logic_borders, grad_dir);
+ edge = aux.*(logic_borders);
 endfunction
--- Practicas/Practica4/deriche.m
+++ Practicas/Practica4/deriche.m
@@ -1,4 +1,4 @@
-function aux = deriche(borders, grad_dir)
+function edge = deriche(borders, grad_dir)
     
     aux = borders;
     [fils, cols] = size(borders)
@@ -7,34 +7,42 @@ function aux = deriche(borders, grad_dir)
     do
       
       aux = borders;
-      i_arrs = find(borders == 255)
+      i_arrs = find(borders == 1);
       [i_s,j_s] = ind2sub(size(borders), i_arrs);
       for ind= 1 : size(i_arrs)(1)
 
         
         switch(grad_dir(i_s(ind, 1),j_s(ind, 1)))
           case {0}
-            v_i = 1;
-            v_j = 0;
-          case {45}
-            v_i = 1;
-            v_j = 1;
-          case {90}
             v_i = 0;
             v_j = 1;
-          case {135}
+          case {45}
             v_i = 1;
             v_j = -1;
+          case {90}
+            v_i = 1;
+            v_j = 0;
+          case {135}
+            v_i = 1;
+            v_j = 1;
          endswitch
         
         if(i_s(ind) + v_i <= fils && i_s(ind) + v_i > 0 && j_s(ind) + v_j <= cols && j_s(ind) + v_j > 0)
-          borders(i_s(ind)+v_i, j_s(ind)+v_j) = 255;
+          if(borders(i_s(ind)+v_i,j_s(ind)+v_j) < 1)
+            borders(i_s(ind)+v_i, j_s(ind)+v_j) = 1;
+          endif
         endif
         
         if(i_s(ind) - v_i <= fils && i_s(ind) - v_i > 0 && j_s(ind) - v_j <= cols && j_s(ind) - v_j > 0)
-          borders(i_s(ind)-v_i, j_s(ind)-v_j) = 255;
+          if(borders(i_s(ind)-v_i,j_s(ind)-v_j) < 1)
+            borders(i_s(ind)-v_i, j_s(ind)-v_j) = 1;
+          endif
         endif        
-      endfor       
+      endfor
+      
+     borders(i_arrs) = 2;   
     until ( aux == borders )
+    edge = borders > 0;
     
+    edge = uint8(edge);
 endfunction
